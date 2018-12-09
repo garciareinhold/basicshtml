7b7096d7e43fe481fada0f6e2cd1968e602feefe
Arturo Garcia Reinhold <arturogreinhold@gmail.com>
2018-12-09 03:36:04 -0300

    first commit on project

---
+++ main.js
@@ -0,0 +1,15 @@
+// document.addEventListener('DOMContentLoaded', function(){
+//
+//   alert("La pagina esta cargada");
+//   console.log("Esto es un console log");
+// })
+
+
+
+bienvenida();
+
+
+function bienvenida() {
+  console.log("hola");
+  alert("gola");
+}
